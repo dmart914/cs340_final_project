4cb9dc78e757abc02fef2aa2c8ae7b29be23dcf8
Lucia <lblackwe@udel.edu>
2015-11-23 21:40:34 +0000

    Added more details to person.php

--- person.php
+++ person.php
@@ -2,14 +2,20 @@
 
 <h2>View Record</h2>
 
+<!-- TODO: List family members, link to family page, list burial information -->
+
 <?php
 	if($_GET["id"])
 	{
-		$first_name = "";
-		$middle_name = "";
-		$last_name = "";
+		$first_name = "unknown";
+		$middle_name = "unknown";
+		$last_name = "unknown";
+		$birthdate = "unknown";
+		$death_date = "unknown";
+		$death_location = "unknown";
+		$cause_of_death = "unknown";
 
-		$statement = $database->prepare('SELECT first_name, middle_name, last_name FROM person WHERE id= :id');
+		$statement = $database->prepare('SELECT * FROM person WHERE id= :id');
 		$statement->bindParam(":id", $_GET["id"]);
 		$statement->execute();
 
@@ -21,10 +27,26 @@
 				$middle_name = ucfirst($row['middle_name']);
 			}
 			$last_name = ucfirst($row['last_name']);
+			if($row['birthdate'])
+			{
+				$birthdate = ucfirst($row['birthdate']);
+			}
+			if($row['death_date'])
+			{
+				$death_date = ucfirst($row['death_date']);
+			}
+			if($row['death_location'])
+			{
+				$death_location = ucfirst($row['death_location']);
+			}
+			if($row['cause_of_death'])
+			{
+				$cause_of_death = ucfirst($row['cause_of_death']);
+			}
 		}
 		echo "<h4 class='subheader'>";
 		echo $first_name;
-		if(strlen($middle_name) > 0)
+		if($middle_name != "unknown")
 		{
 			echo " ".$middle_name;
 		}
@@ -32,9 +54,14 @@
 		echo "</h4>";
 
 		echo "<ul>";
-			echo "<li>Born: </li>";
-			echo "<li>Died: </li>";
-			echo "<li>Cause of death: </li>";
+			echo "<li>Born: ".$birthdate."</li>";
+			echo "<li>Died: ".$death_date;
+			if($death_location != "unknown")
+			{
+				echo " in ".$death_location;
+			}
+			echo "</li>";
+			echo "<li>Cause of death: ".$cause_of_death."</li>";
 			echo "<li>&lt;etc&gt;</li>";
 		echo "</ul>";
 	}
