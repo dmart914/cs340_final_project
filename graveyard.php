2701f9485bca8e6dbc7f3b2879c18c991204d38b
Lucia <lblackwe@udel.edu>
2015-11-24 00:29:30 +0000

    Added details to graveyard info page

--- graveyard.php
+++ graveyard.php
@@ -11,7 +11,7 @@
 		$city = "";
 		$state = "";
 		$zip = "";
-		$contact = "";
+		$contact = "Unavailable";
 
 		$statement = $database->prepare('SELECT * FROM graveyard WHERE id= :id');
 		$statement->bindParam(":id", $_GET["id"]);
@@ -49,7 +49,70 @@
 		echo $name;
 		echo "</h4>";
 
-		echo "<p>&lt;Graveyard information here&gt;</p>";
+		echo "<p>";
+			if(strlen($street1) > 0)
+			{
+				echo $street1."<br>";
+			}
+			if(strlen($street2) > 0)
+			{
+				echo $street2."<br>";
+			}
+			echo $city;
+			if(strlen($city) > 0 && strlen($state) > 0)
+			{
+				echo ", ";
+			}
+			echo $state." ";
+			echo $zip;
+		echo "</p>";
+
+		echo "<p>Contact: ".$contact."</p>";
+
+		echo "<h5>Registered occupants</h5>";
+
+		$statement = $database->prepare(
+			'SELECT p.id, p.last_name, p.first_name, p.middle_name, plot.x_coord, plot.y_coord
+			FROM person AS p, plot, graveyard AS g
+			WHERE g.id= :id
+				AND plot.graveyard_id = g.id
+				AND p.plot_id = plot.id
+			GROUP BY p.last_name
+			ORDER BY p.last_name
+			LIMIT 10');
+		$statement->bindParam(":id", $_GET["id"]);
+		$statement->execute();
+
+		echo "<table>";
+		while($row = $statement->fetch())
+		{
+			echo "<tr>";
+				echo "<td>";
+					echo "<a href='person.php?id=".$row['id']."'>";
+					echo ucfirst($row['last_name']);
+					if($row['first_name'])
+					{
+						echo ", ".ucfirst($row['first_name']);
+					}
+					if($row['middle_name'])
+					{
+						echo " ".ucfirst($row['middle_name']);
+					}
+					echo "</a>";
+				echo "</td>";
+				echo "<td>";
+					if($row['x_coord'] && $row['y_coord'])
+					{
+						echo "(".$row['x_coord'].", ".$row['y_coord'].")";
+					}
+					else
+					{
+						echo "Coordinates unavailable.";
+					}
+				echo "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
 	}
 ?>
 
