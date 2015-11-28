1b4a8094a7131ea5b66980c5ca32ee9f66bedd52
Lucia <lblackwe@udel.edu>
2015-11-28 22:13:00 +0000

    Added graveyard search form

--- search2.php
+++ search2.php
@@ -13,6 +13,7 @@
 <?php
 	if($_GET["find"] == "person")
 	{
+		echo "<h4 class='subheader'>People</h4>";
 		echo "<form>";
 			echo "<div class='row'>";
 				echo "<div class='small-2 columns'>";
@@ -61,8 +62,10 @@
 		echo "</form>";
 		echo "<a href='#' class='button'>Search</a>";
 	}
+
 	elseif ($_GET["find"] == "family")
 	{
+		echo "<h4 class='subheader'>Families</h4>";
 		echo "<form>";
 			echo "<div class='row'>";
 				echo "<div class='small-2 columns'>";
@@ -81,10 +84,54 @@
 		echo "</form>";
 		echo "<a href='#' class='button'>Search</a>";
 	}
+
 	elseif ($_GET["find"] == "graveyard")
 	{
-		# code...
+		echo "<h4 class='subheader'>Graveyards and Cemeteries</h4>";
+		echo "<form>";
+			echo "<div class='row'>";
+				echo "<div class='small-4 columns'>";
+					echo "<label>Name";
+					echo "<input type='text'>";
+					echo "</label>";
+				echo "</div>";
+				echo "<div class='small-8 columns'>";
+				echo "</div>";
+			echo "</div>";
+
+			echo "<div class='row'>";
+				echo "<div class='small-5 columns'>";
+					echo "<label>Street address";
+					echo "<input type='text'>";
+					echo "</label>";
+				echo "</div>";
+				echo "<div class='small-7 columns'>";
+				echo "</div>";
+			echo "</div>";
+
+			echo "<div class='row'>";
+				echo "<div class='small-3 columns'>";
+					echo "<label>City";
+					echo "<input type='text'>";
+					echo "</label>";
+				echo "</div>";
+				echo "<div class='small-2 columns'>";
+					echo "<label>State";
+					echo "<input type='text'>";
+					echo "</label>";
+				echo "</div>";
+				echo "<div class='small-2 columns'>";
+					echo "<label>Zip code";
+					echo "<input type='text'>";
+					echo "</label>";
+				echo "</div>";
+				echo "<div class='small-5 columns'>";
+				echo "</div>";
+			echo "</div>";
+		echo "</form>";
+		echo "<a href='#' class='button'>Search</a>";
 	}
+
 	else
 	{
 		echo "<dl><dt>An error occurred.</dt><dd>Please <a href='search.php'>try again</a>.</dd></dl>";
