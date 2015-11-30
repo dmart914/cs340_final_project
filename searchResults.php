caff2613a2e0054365788b40560878fdb64c5b16
Lucia <lblackwe@udel.edu>
2015-11-30 21:42:28 +0000

    Working on nav bar search

--- searchResults.php
+++ searchResults.php
@@ -1,9 +1,26 @@
 <?php include("layouts/top.php"); ?>
 
-<h3>Search Results</h3>
+<h3>Search Records</h3>
 
 <!-- <h4 class="subheader">&lt;BLURB HERE&gt;</h4> -->
 
-<h5>&lt;Header&gt;</h5>
+<?php
+	$searchString = "";
+	if($_POST['searchString'])
+	{
+		$searchString = $_POST['searchString'];
+	}
+
+	if(!$_POST['searchString'])
+	{
+		# Handle empty search string here
+	}
+
+	else
+	{
+		echo "<h5>Results for <i>".$searchString."</i></h5>";
+	}
+	
+?>
 
 <?php include("layouts/bottom.php"); ?>
